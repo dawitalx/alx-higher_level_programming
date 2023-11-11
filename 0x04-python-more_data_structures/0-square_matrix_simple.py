6282ee712c6205d36e338fdba4a46ac29902771f
dawitalx <dawitvid@gmail.com>
2023-11-11 03:07:50 -0800

    code updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x:x*x,row)) for row in matrix])
+    """ Compute the square of numbers inside a matrix"""
+    return ([list(map(lambda x:x*x,row)) for row in matrix]) 
+ 
